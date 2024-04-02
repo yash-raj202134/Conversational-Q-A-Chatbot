796d728316c37fba5686002da0ea0618d38b500c
yash-raj202134 <yashraj3376@gmail.com>
2024-04-02 16:23:01 +0530

    requirements setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
+    
+    response=chat.send_message(question,stream=True)
+    return response
+
